b5a6e70844accbf187ae0f80d0341a20a555e6a1
milesmathews70 <milesm2@illinois.edu>
2018-12-03 15:32:17 -0600

    Added comments and some spacing.

--- src/com/company/CodeBreaker.java
+++ src/com/company/CodeBreaker.java
@@ -3,19 +3,55 @@ import java.util.*;
 
 
 public class CodeBreaker {
+
+    /**
+     * allCombinations is the list of every single combination.
+     */
     private static List<String> allCombinations = new ArrayList<String>();
+
+    /**
+     * The array of different letters.
+     */
     private static final String[] letters = new String[6];
+    /**
+     * The letters that we use that is assigned to each color.
+     */
     private static final String G = "green";
+
     private static final String U = "blue";
+
     private static final String R = "red";
+
     private static final String Y = "yellow";
+
     private static final String O = "orange";
+
     private static final String W = "white";
+
+    /**
+     * The number turns you are allowed.
+     */
     private static int numOfTurns = 10;
+
+    /**
+     * Our player's name.
+     */
     private static String player;
+
+    /**
+     * The secret code that is randomly created from the allCombinations list.
+     */
     private static String secretC;
+
+    /**
+     * The secret code in an array of Strings.
+     */
     private static String[] secretCode = new String[4];
 
+    /**
+     * The constructor for the game.
+     * @param p The player's name.
+     */
     public CodeBreaker(String p) {
         player = p;
         Random obj = new Random();
@@ -27,6 +63,9 @@ public class CodeBreaker {
         getGuess();
     }
 
+    /**
+     * Shows the game layout, which is comprised of the rules.
+     */
     public static void showGame() {
         System.out.println("---");
         System.out.println("Welcome to Mastermind, " + player + "!");
@@ -41,12 +80,18 @@ public class CodeBreaker {
         showAllRules();
     }
 
+    /**
+     * Shows the rules, which is comprised of the game rules and player rules.
+     */
     public static void showAllRules() {
         System.out.println("Displaying Rules to the Game Mastermind:");
         showGameRules();
         showPlayerRules();
     }
 
+    /**
+     * The game rules, the rules of the game.
+     */
     public static void showGameRules() {
         System.out.println("---");
         System.out.println("This is a Board game created by Mordecai Meirowitz.");
@@ -61,6 +106,9 @@ public class CodeBreaker {
         System.out.println("---");
     }
 
+    /**
+     * The player rules, the rules which the player obeys.
+     */
     public static void showPlayerRules() {
         System.out.println(player + ", you are playing as the Codebreaker.");
         System.out.println("---");
@@ -84,6 +132,9 @@ public class CodeBreaker {
         System.out.println("Have fun!");
     }
 
+    /**
+     * The call where we get the player's guess's. If the guess is incorrect, the method will call itself again.
+     */
     public static void getGuess() {
         if (numOfTurns == 0) {
             finishGame();
@@ -107,6 +158,10 @@ public class CodeBreaker {
             }
         }
     }
+
+    /**
+     * Method for getting all the different possible combinations.
+     */
     public static void getAllCombinations() {
         for (int i = 0; i < 6; i++) {
             for (int j = 0; j < 6; j++) {
@@ -120,6 +175,11 @@ public class CodeBreaker {
         }
     }
 
+    /**
+     * Checks if the player's guess is correct. If player is correct, they win.
+     * Otherwise, guess is sent to the checkCorrect method.
+     * @param guess The guess, comes in a String of 4 colors.
+     */
     public static void isCorrect(String guess) {
         if (guess.equals(secretC)) {
             System.out.println("---");
@@ -134,6 +194,11 @@ public class CodeBreaker {
         }
     }
 
+    /**
+     * Checks "how correct" the player's guess is.
+     * @param g The player's guess.
+     * @return returns a string of pegs identifying which color is correct or not.
+     */
     public static String checkCorrect(String g) {
         String pegs = "";
         String[] guess = new String[4];
@@ -170,12 +235,18 @@ public class CodeBreaker {
 
     }
 
+    /**
+     * When the player runs out of turns, they lose.
+     */
     public static void finishGame() {
         System.out.println("You lost!");
         System.out.println("---");
         System.out.println("The secret code was: " + secretC);
     }
 
+    /**
+     * When the player wins, the console says how many turns they had left.
+     */
     public static void winGame() {
         System.out.println(player + " won with " + numOfTurns + " turns left!");
     }
